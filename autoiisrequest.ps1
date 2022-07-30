296a2cbe6dd0e26db4b6961821aaf5ee83771f7b
Credentive <66961157+Credentive-Sec@users.noreply.github.com>
2022-07-30 10:32:17 -0400

    Update Help text

--- autoiisrequest.ps1
+++ autoiisrequest.ps1
@@ -1,4 +1,6 @@
 param (
+    # Note to CA Administrators, these variables can be pre-initialized in your environment to simplify the execution of the script for your 
+    # IIS Admins
     [String]$hostname = "",
     [String]$location = "",
     [String]$site = "",
@@ -10,17 +12,21 @@ param (
 )
 
 [String[]] $help_text = @"
-AutoIISRequest.ps1 [-hostname <Hostname>] [-location <Directory>] [-template <Template Name>] [-ca_server <CA Name>] [-request] [-install] [-force]
-hostname: The hostname that should be present in the request. If this option is absent, the FQDN will be used.
-location: A directory to save artifacts in (eg. requests inf, csr, crt). If this option is absent, `$HOME will be used.
-site: Specify the site to install the certificate on. If you don't specify a site, and there is only one, the cert will be installed there. If there is more than one, the command will exit with an error.
-template: Specify the certificate template to be used.
-ca_server: The name of the CA Server to use. This only needs to be specified if the template can be issued from more than one CA. If this is the case, the script will provide the CA names.
-request: Create a private key and submit a certificate request to the CA server.
-install: Retrieve and install a certificate after the certificate is issued.
-force: Overwrite artifact files if they exist.
+AutoIISRequest.ps1 [optional parameters] [-request | -install] [-force]
 
-NOTE: either "request" or "install" (but not both) must be specified on the commandline.
+Mandatory Parameters
+* request: Create a private key and submit a certificate request to the CA server.
+* install: Retrieve and install a certificate after the certificate is issued.
+
+NOTE: either "request" or "install" (but not both) must be specified on the commandline. All other parameters are optional
+
+Optional Parameters
+* hostname: The hostname that should be present in the request. If this option is absent, the FQDN will be used.
+* location: A directory to save artifacts in (eg. requests inf, csr, crt). If this option is absent, `$HOME will be used.
+* site: Specify the site to install the certificate on. If you don't specify a site, and there is only one, the cert will be installed there. If there is more than one, the command will exit with an error.
+* template: Specify the certificate template to be used.
+* ca_server: The name of the CA Server to use. This only needs to be specified if the template can be issued from more than one CA. If this is the case, the script will provide the CA names.
+* force: Overwrite artifact files if they exist.
 "@
 
 #
